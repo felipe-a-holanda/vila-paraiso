5f81c2f714da6f1e613f09be1579605a9952e95b
Felipe Holanda <flp9001@gmail.com>
2017-06-30 19:29:30 -0300

    Actviate virtualenv on wsgi.py

--- vilaparaiso/vilaparaiso/wsgi.py
+++ vilaparaiso/vilaparaiso/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vilaparaiso.settings")
+
+# Activate your virtual env
+activate_env=os.path.expanduser("~/vilaparaiso.org/env/bin/activate_this.py")
+exec(open(activate_env).read(),dict(__file__=activate_env))
 
 application = get_wsgi_application()
