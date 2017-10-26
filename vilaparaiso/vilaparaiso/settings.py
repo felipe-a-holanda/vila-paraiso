8c0843b265103fe51a184907a3505044ae0c156b
Felipe Holanda <flp9001@gmail.com>
2017-10-26 17:55:46 -0200

    Removed staticfiles dir

--- vilaparaiso/vilaparaiso/settings.py
+++ vilaparaiso/vilaparaiso/settings.py
@@ -166,9 +166,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
